f1fbf114af483bca30f822e6614d0d99ec127f82
Itzrrj <shresthxraj@gmail.com>
2025-01-12 22:27:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,9 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
+import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
 class Bot(Client):
